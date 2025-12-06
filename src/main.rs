ab498da87547a389f43517a564a5a7186ffec356
yechenkai <yechenkai@foxmail.com>
2025-12-06 19:22:16 +0800

    主功能抽取一层模块，清理main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::thread::JoinHandle;
-use std::{io, thread};
 use clap::Parser;
 
-use crate::ip_box::next_ip;
-
-mod arp_core;
-mod ip_box;
+mod scan_master;
 
 #[derive(Parser)]
 #[command(version, author, about, long_about = None)]
@@ -18,65 +13,11 @@ fn main() {
     let cli = Cli::parse();
     let cidr = &cli.cidr;
     println!("Use CIDR: {cidr}");
-    let first_ip_addr_str = ip_box::first_ip(cidr).expect("No avalivable addr.");
-    let mut ip_string = first_ip_addr_str;
-    let mut handle_vec: Vec<JoinHandle<()>> = Vec::new();
-    loop {
-        //println!("current_ip: {}", ip_string);
-        let ip_string_cur_temp = ip_string.clone();
-        let handle = thread::spawn(
-            move || match check_ip_exist(&(ip_string_cur_temp.clone())) {
-                Ok(result) => {
-                    if result.exist {
-                        println!("{} --- {}", result.ip, result.mac);
-                    }
-                }
-                Err(e) => {
-                    println!("Error: {e}");
-                }
-            },
-        );
-        handle_vec.push(handle);
 
-        match next_ip(cidr, &ip_string) {
-            Some(ip) => {
-                ip_string = ip;
-            }
-            None => {
-                break;
-            }
+    let res_vec = scan_master::scan_by_arp(cidr).unwrap();
+    for res_elem in res_vec {
+        if res_elem.exist {
+            println!("{} --- {}", res_elem.ip, res_elem.mac);
         }
     }
-    for handle in handle_vec {
-        if let Err(e) = handle.join() {
-            println!("Thread handle error: {:?}", e);
-        }
-    }
-}
-
-struct IpCheckResult {
-    ip: String,
-    mac: String,
-    exist: bool,
-}
-
-fn check_ip_exist(ip_str: &str) -> io::Result<IpCheckResult> {
-    let mut result = IpCheckResult {
-        ip: ip_str.to_string(),
-        mac: String::new(),
-        exist: false,
-    };
-    let ip_addr = arp_core::parse_ip(ip_str)?;
-    //println!("目标 IP: {} (0x{:08X})", ip_str, ip_addr);
-
-    if let Ok(mac) = arp_core::get_mac_address(ip_addr) {
-        result.mac = format!(
-            "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
-            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
-        )
-        .to_string();
-        result.exist = true;
-    }
-
-    Ok(result)
 }
---
+++ src/scan_master.rs
@@ -0,0 +1,78 @@
+use std::io;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::thread::JoinHandle;
+
+mod arp_core;
+mod ip_box;
+
+pub fn scan_by_arp(cidr: &str) -> io::Result<Vec<IpCheckResult>> {
+    let avaliable_node_list: Arc<Mutex<Vec<IpCheckResult>>> = Arc::new(Mutex::new(Vec::new()));
+    let first_ip_addr_str = ip_box::first_ip(cidr).expect("No avalivable addr.");
+    let mut ip_string = first_ip_addr_str;
+    let mut handle_vec: Vec<JoinHandle<()>> = Vec::new();
+    loop {
+        //println!("current_ip: {}", ip_string);
+        let avaliable_node_list_shared_clone = Arc::clone(&avaliable_node_list);
+        let ip_string_cur_temp = ip_string.clone();
+        let handle = thread::spawn(
+            move || match check_ip_exist(&(ip_string_cur_temp.clone())) {
+                Ok(result) => {
+                    if result.exist {
+                        let mut avaliable_node_list_guard =
+                            avaliable_node_list_shared_clone.lock().unwrap();
+                        avaliable_node_list_guard.push(result);
+                        //println!("{} --- {}", result.ip, result.mac);
+                    }
+                }
+                Err(e) => {
+                    println!("Error: {e}");
+                }
+            },
+        );
+        handle_vec.push(handle);
+
+        match ip_box::next_ip(cidr, &ip_string) {
+            Some(ip) => {
+                ip_string = ip;
+            }
+            None => {
+                break;
+            }
+        }
+    }
+    for handle in handle_vec {
+        if let Err(e) = handle.join() {
+            println!("Thread handle error: {:?}", e);
+        }
+    }
+    Ok(avaliable_node_list.lock().unwrap().to_vec())
+}
+
+#[derive(Clone)]
+pub struct IpCheckResult {
+    pub ip: String,
+    pub mac: String,
+    pub exist: bool,
+}
+
+fn check_ip_exist(ip_str: &str) -> io::Result<IpCheckResult> {
+    let mut result = IpCheckResult {
+        ip: ip_str.to_string(),
+        mac: String::new(),
+        exist: false,
+    };
+    let ip_addr = arp_core::parse_ip(ip_str)?;
+    //println!("目标 IP: {} (0x{:08X})", ip_str, ip_addr);
+
+    if let Ok(mac) = arp_core::get_mac_address(ip_addr) {
+        result.mac = format!(
+            "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+        )
+        .to_string();
+        result.exist = true;
+    }
+
+    Ok(result)
+}
